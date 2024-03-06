eacb8b8da818f3e565d8fe3469347c269df655e9
vvinRAR <vincent.blaesser@gmail.com>
2024-03-07 00:51:49 +0100

    Convert function/Solve list
    Funktionen um Einputstring in Liste zu übertragen

--- modules/main.py
+++ modules/main.py
@@ -2,6 +2,9 @@ from calculator import *
 
 myCalculator = Calculator()
 
-myCalculator.inputstring = "12/44"
-#print(myCalculator.inputstring)
+myCalculator.inputstring = "sin(12)+3*7-arccos(43)"
+myCalculator.activeMode.solve()
+myCalculator.inputstring = "745*arctan(12)/32+3*7-arccos(43)-tan(3)"
+myCalculator.activeMode.solve()
+myCalculator.inputstring = "25*3+27/34-17"
 myCalculator.activeMode.solve()
--- modules/mode_computation.py
+++ modules/mode_computation.py
@@ -33,30 +33,86 @@ class Mode_Computation(Mode):
         
         Input.add(button.value)
 
-    
-    def solve(self):
-        operatorliste = ["+", "-", "*", "/"]
-        Speicher = []
-        i=0
-        while i < len(self.calculator.inputstring):
-            if self.calculator.inputstring[i] in operatorliste:
-                x=i
-                a = int(''.join(Speicher))
-                print(a)
-                Speicher=[]
-                i+=1
+
+    # def initial(self, long_operatorlist):
+    #     long_operatorlist_initial = []
+    #     for element in long_operatorlist:
+    #         long_operatorlist_initial.append(element[0])
+    #     return long_operatorlist_initial
+        
+    def slicer(self, long_operatorlist):
+        long_operatorlist_slices = []
+        for element in long_operatorlist:
+            for part in element:
+                long_operatorlist_slices.append(part)
+        return long_operatorlist_slices
+
+
+    def Convert(self, inputstring, operatorlist, long_operatorlist):
+        # long_operatorlist_initial = self.initial(long_operatorlist)
+        # print(long_operatorlist_initial)
+        long_operatorlist_slices = self.slicer(long_operatorlist)
+        Solve_list = []
+        memory = []
+        Test = True
+        for element in inputstring:
+            if element in operatorlist:
+                Solve_list.append(''.join(memory))
+                memory = []
+                Solve_list.append(element)
+            # elif element in long_operatorlist_initial:
+            #     continue
+            elif element in long_operatorlist_slices:
+                if Test:
+                    Solve_list.append(''.join(memory))
+                    memory = []
+                    Test=False
+                else:
+                    Test=False
+                memory.append(element)
             else:
-                Speicher.append(self.calculator.inputstring[i])
-                i+=1
-        b = int(''.join(Speicher))
-        if self.calculator.inputstring[x] == "+":
-            print(a+b)
-        elif self.calculator.inputstring[x] == "-":
-            print(a-b)
-        elif self.calculator.inputstring[x] == "*":
-            print(a*b)
-        elif self.calculator.inputstring[x] == "/":
-            print(a/b)
+                try:
+                    if memory[0] in long_operatorlist_slices:
+                        Solve_list.append(''.join(memory))
+                        memory = []
+                        Test=True
+                    else:
+                        Test=True
+                except:
+                    Test=True
+                memory.append(element)
+        Solve_list.append(''.join(memory))
+        while '' in Solve_list:
+            Solve_list.remove('')
+        return Solve_list
+
+
+
+    def solve(self):
+        operatorlist = ["+", "-", "*", "/", ")"]
+        long_operatorlist = ["sin(", "arcsin(", "cos(", "arccos(", "tan(", "arctan("]
+        Solve_list = self.Convert(self.calculator.inputstring, operatorlist, long_operatorlist)
+        print(Solve_list)
+        # Speicher = []
+        # i=0
+        # while i < len(self.calculator.inputstring):
+        #     if self.calculator.inputstring[i] in operatorliste:
+        #         x=i
+        #         a = int(''.join(Speicher))
+        #         Speicher=[]
+        #         i+=1
+        #     else:
+        #         Speicher.append(self.calculator.inputstring[i])
+        #         i+=1
+        # b = int(''.join(Speicher))
+        # if self.calculator.inputstring[x] == "+":
+        #     print(a+b)
+        # elif self.calculator.inputstring[x] == "-":
+        #     print(a-b)
+        # elif self.calculator.inputstring[x] == "*":
+        #     print(a*b)
+        # elif self.calculator.inputstring[x] == "/":
+        #     print(a/b)
 
 
 
